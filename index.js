58b84ce04ecf720e16ac0eaaaeb1e6632e058b37
lipeilin375 <74428719+lipeilin375@users.noreply.github.com>
2023-02-26 17:20:57 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+window.onload = function () {
+    window.open('https://jinjieup-my.sharepoint.com/:p:/g/personal/lipeilin_jinjieup_ml/EXirRyHF-etEro9GVNNS6u0BnV_97YOi5VzRm-7FsnAjBg?e=MqB74d','_self')
+}
